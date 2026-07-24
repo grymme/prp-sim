862250840dd97da81a0ac56b761a540ac59aad9c
Henrik Envall <henrik.envall@gmail.com>
2026-07-24 12:41:53 +0200

    fix: move tests to correct packages, fix relative paths

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,7 +10,7 @@ func TestLoad(t *testing.T) {
 }
 
 func TestLoadValid(t *testing.T) {
-	cfg, err := Load("../config.yaml")
+	cfg, err := Load("../../config.yaml")
 	if err != nil {
 		t.Fatalf("unexpected error: %v", err)
 	}
